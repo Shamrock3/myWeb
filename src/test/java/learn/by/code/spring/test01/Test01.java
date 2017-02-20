719140843af5a8108ba58747e7d71667bc489010
liang <1103347606@qq.com>
2017-02-20 17:06:24 +0800

    Initial commit

---
+++ src/test/java/learn/by/code/spring/test01/Test01.java
@@ -0,0 +1,14 @@
+package learn.by.code.spring.test01;
+
+import org.junit.Test;
+
+/**
+ * Created by liang on 2017/2/20.
+ */
+public class Test01 {
+    @Test
+    public void getBean()
+    {
+        
+    }
+}
